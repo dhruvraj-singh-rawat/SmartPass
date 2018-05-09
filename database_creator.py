113987ea8e5499a1b3c6896bc757571c5e111c35
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-05-09 19:52:38 +0530

    Added: Table for Travel History and for Travel Statement

--- database_creator.py
+++ database_creator.py
@@ -32,6 +32,48 @@ class UserTable(Base):
            'typeCard'         : self.typeCard
        }
 
+class TravelHistory(Base):
+    __tablename__ = 'Travel History'
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(250))
+    trips_added = Column(String(250))
+    trip_taken = Column(String(250))
+
+    holder_id = Column(Integer,ForeignKey('user_table.id'))
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+          
+           'email'           : self.email,
+           'trips_added'         : self.trips_added,
+           'trip_taken'              : self.trip_taken,
+       }
+
+class TravelStatement(Base):
+    __tablename__ = 'Travel Statement'
+
+
+    
+    id = Column(Integer, primary_key = True)    
+    balance_trip = Column(String(8))    
+    email = Column(String(250))
+
+    holder_id = Column(Integer,ForeignKey('user_table.id'))
+    
+
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {          
+           
+           'balance_trip'         : self.balance_trip,
+           'email'         : self.email,
+
+       }
 
 
 
