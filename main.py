37889f346180b25bc239b8bd2a934e194246d539
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-05-10 20:03:29 +0530

    Added: API Endpoints for Scanner

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from flask_admin import Admin 
 from flask_admin.contrib.sqla import ModelView
-
+import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///digitalpass.db'
 app.config['SECRET_KEY']='mysecret'
@@ -43,6 +43,46 @@ class statement(db.Model):
 admin.add_view(ModelView(user,db.session))
 admin.add_view(ModelView(history,db.session))
 admin.add_view(ModelView(statement,db.session))
+
+
+# API IMPLEMENTATION 
+
+@app.route("/api_responce", methods = ['GET', 'POST'])
+def api_responce():
+
+    if request.method=='POST':
+
+        rfidno=request.args.get('rfid_no')
+
+    
+        user_balance = db.session.query(statement).filter_by(rfidno=rfidno).one()
+
+        balance_available=int(user_balance.balance_trip)
+        
+
+        if balance_available>0:
+            balance_available=balance_available-1
+            user_balance.balance_trip=balance_available
+            db.session.commit()
+            
+            output={
+                'status':1,
+
+            }
+            return json.dumps(output)
+        else:
+            output={
+                'status':0,
+            }
+            return json.dumps(output)
+
+
+
+    elif request.method=='GET':
+
+        response=make_response(json.dumps('GET is working',200))
+        response.headers['Content-Type']='application/json'
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
